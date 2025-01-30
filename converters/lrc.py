dcb6804c56212bb0fdb07947d3e51c46d659294d
drosoCode <contact@thomasz.me>
2025-01-31 00:21:19 +0100

    initial lrc converter

---
+++ converters/lrc.py
@@ -0,0 +1,104 @@
+from models.events import *
+from models.ultrastar import UltrastarFile
+
+import re
+from math import floor
+from typing import List
+
+# sometimes lrc are not well synced
+# we can use this to check beforehand: https://mikezzb.github.io/lrc-player/
+
+class LrcConverter:
+    LRC_REG = re.compile(r"<(\d{2}:\d{2}.\d{2})> +([A-zÀ-ú'’,]+)")
+    LRC_LINE_REG = re.compile(r"([A-zÀ-ú'’,]+) *")
+    bpm = 250
+
+    def __init__(self, bpm=250):
+        self.bpm = bpm
+
+    def _parse_time(self, time_str):
+        # Convert a time string in the format MM:SS.MS to the number of seconds
+        minutes, seconds = time_str.split(":")
+        minutes = int(minutes)
+        seconds, milliseconds = map(int, seconds.split("."))
+        
+        total_seconds = minutes * 60 + seconds + milliseconds / 100
+        return total_seconds
+    
+    def _sec_to_bpm(self, val):
+        return floor(val/60*self.bpm*4) # no idea why, but x4 fixes all sync problems
+
+    def convert(self, lyrics: List[str]) -> UltrastarFile:
+        ultrastar = UltrastarFile()
+        ultrastar.bpm = self.bpm
+        ret = []
+        is_gap_set = False
+        
+        next_break = 0
+        lrc_len = len(lyrics)
+        for i in range(lrc_len):
+            # iterate over lines of text
+            is_word_by_word = True
+            avg_sec_by_word = 0
+
+            if len(lyrics[i]) > 11 and lyrics[i][0] == "[" and lyrics[i][9] == "]":
+                start = self._parse_time(lyrics[i][1:9])
+                txt = self.LRC_REG.findall(lyrics[i][11:])
+                if len(txt) == 0:
+                    txt = self.LRC_LINE_REG.findall(lyrics[i][11:])
+                    is_word_by_word = False
+                #example text: <00:10.91> Yeah, <00:11.18>   <00:11.22> I <00:11.34>   <00:11.48> got <00:11.56>   <00:11.66> voices 
+
+                # iterate over words to count letters, if empty skip the line
+                if is_word_by_word:
+                    nb_letters = sum([len(x[1]) for x in txt])
+                else:
+                    nb_letters = sum([len(x) for x in txt])
+                if nb_letters == 0:
+                    continue
+
+                if not is_word_by_word:
+                    if i+1 < lrc_len and len(lyrics[i+1]) >= 10 and lyrics[i+1][0] == "[" and lyrics[i+1][9] == "]":
+                        next_start = self._parse_time(lyrics[i+1][1:9])
+                    line_duration = (next_start-start)*0.9 # use 95% of line length for word, and keep 5% for the break
+                    next_break = (next_start-start)*0.1
+                    avg_sec_by_word = line_duration/nb_letters
+
+                # processing =================
+                if not is_gap_set:
+                    # if this is the first line, add the GAP
+                    is_gap_set = True
+                    ultrastar.gap = floor(start)
+                else:
+                    ret.append(UltrastarBreak(floor(self._sec_to_bpm(next_break))))
+                
+                txt_len = len(txt)
+                total_sec = start
+                for j in range(txt_len):
+                    if is_word_by_word:
+                        word = txt[j][1]
+                        start_sec = self._parse_time(txt[j][0])
+                        if j+1 < txt_len:
+                            # get start of next word
+                            next_sec = self._parse_time(txt[j+1][0])
+                        elif i+1 < lrc_len and len(lyrics[i+1]) >= 10 and lyrics[i+1][0] == "[" and lyrics[i+1][9] == "]":
+                            # if not available, take start of next line
+                            next_sec = self._parse_time(lyrics[i+1][1:9])
+                            next_break = ((next_sec-start_sec)*0.2) # use the remaining 20% of the timeframe for the break (see duration_sec below)
+                        else:
+                            # if not available, use an arbitrary time of 3 sec
+                            next_sec = start_sec + 3
+                        duration_sec = (next_sec-start_sec)*0.8 # use only 80% of the timeframe as we also need "blank" space between words
+                    else:
+                        word = txt[j]
+                        start_sec = total_sec
+                        duration_sec = len(word)*avg_sec_by_word
+                        total_sec += duration_sec
+                        duration_sec *= 0.8 # use only 80% of the timeframe as we also need "blank" space between words
+                    
+                    
+                    # StartBeat, Length, Pitch, Text
+                    ret.append(UltrastarText(time=self._sec_to_bpm(start_sec), length=self._sec_to_bpm(duration_sec), pitch=0, start_space=True, text=word))
+
+        ultrastar.events = ret
+        return ultrastar
